0cdee00c1a0d8e319a1573903126af7620b91b61
Udit Padhan <uditpadhan98@gmail.com>
2024-06-16 08:50:04 +0530

    cors change

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const authRoutes = require("./routes/auth.js")
 const listingRoutes = require("./routes/listing.js")
 const userRoutes = require("./routes/user.js")
 
-app.use(cors({ origin: "*", credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
