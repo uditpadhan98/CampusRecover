6194c662281bef890452378ccf54b212af4f414e
Udit Padhan <uditpadhan98@gmail.com>
2024-09-04 15:55:05 +0530

    new changes

--- server/index.js
+++ server/index.js
@@ -9,13 +9,23 @@ const listingRoutes = require("./routes/listing.js")
 const userRoutes = require("./routes/user.js")
 
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+// credentials: true allows your server to accept cookies, authentication headers, 
+// or client certificates in cross-origin requests.
 app.use(express.json());
+// express.json() is a built-in middleware function in Express that parses the incoming 
+// request body as JSON and makes it available on the req.body object.
 app.use(express.static("public"));
+// serves static files like HTML, CSS, JavaScript, and images from the public directory.
 
 /* ROUTES */
-app.use("/auth", authRoutes)
-app.use("/items", listingRoutes)
+app.use("/auth", authRoutes) //All routes in authRoutes are prefixed with /auth
+app.use("/items", listingRoutes) //All routes in listingRoutes are prefixed with /items.
 app.use("/users", userRoutes)
+
+// req (Request Object): Contains information about the client's HTTP request, 
+// such as URL, query parameters, headers, and body.
+// res (Response Object): Used to send the server's response back to the client, 
+// including setting the status code, headers, and body of the response.
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
--- server/routes/listing.js
+++ server/routes/listing.js
@@ -1,20 +1,7 @@
 const router = require("express").Router();
-// const multer = require("multer");
 
 const Listing = require("../models/Listing");
 const User = require("../models/User");
-
-/* Configuration Multer for File Upload */
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, "public/uploads/"); // Store uploaded files in the 'uploads' folder
-//   },
-//   filename: function (req, file, cb) {
-//     cb(null, file.originalname); // Use the original file name
-//   },
-// });
-
-// const upload = multer({ storage });
 
 /* CREATE LISTING */
 router.post("/create", async (req, res) => {
@@ -94,8 +81,6 @@ router.delete("/:deleteId", async (req, res) => {
   try {
     const { deleteId } = req.params;
     const { currentUserId, creatorId } = req.body;
-    // console.log(currentUserId,creatorId);
-    // console.log(listingId);
     if (currentUserId === creatorId) {
       await Listing.findByIdAndDelete(deleteId);
       return res
